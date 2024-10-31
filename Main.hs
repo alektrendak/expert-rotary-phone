78183a16230e3e3dad259c8dae19cb63c8122035
alektrendak <alek.trendak@gmail.com>
2024-10-31 13:41:25 +0100

    Interpreter logic. It interprets

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
-import Data.Map (Map, fromList, insert)
+import Data.Map (Map, findWithDefault, fromList, insert, (!))
+import Data.Typeable (typeOf)
 import System.Environment (getArgs)
 import System.IO (readFile)
 
@@ -11,8 +12,7 @@ main = do
             bracketMap <- case parseBrackets code of
                 Just m -> return m
                 Nothing -> fail "Unbalanced brackets"
-            putStrLn code
-            print bracketMap
+            run code bracketMap
         _ -> putStrLn "Usage: runhaskell Main.hs <filename>"
 
 parseBrackets :: String -> Maybe (Map Int Int)
@@ -27,3 +27,28 @@ parseBrackets code = goParseBrackets code 0 [] (fromList [])
                 [] -> Nothing
                 (j:stack') -> goParseBrackets cs (i + 1) stack' (insert i j (insert j i m))
             | otherwise = goParseBrackets cs (i + 1) stack m
+
+run :: String -> Map Int Int -> IO ()
+run code bracketMap = goRun code 0 0 (fromList []) bracketMap
+    where
+        goRun :: String -> Int -> Int -> Map Int Int -> Map Int Int -> IO ()
+        goRun code i _ _ _
+            | i == length(code) = return ()
+        goRun code i p m bracketMap
+            | code!!i == '>' = goRun code (i + 1) (p + 1) m bracketMap
+            | code!!i == '<' = goRun code (i + 1) (p - 1) m bracketMap
+            | code!!i == '+' = goRun code (i + 1) p (insert p ((findWithDefault 0 p m) + 1) m) bracketMap
+            | code!!i == '-' = goRun code (i + 1) p (insert p ((findWithDefault 0 p m) - 1) m) bracketMap
+            | code!!i == '.' = do
+                putChar (toEnum (m ! p))
+                goRun code (i + 1) p m bracketMap
+            | code!!i == ',' = do
+                c' <- getChar
+                goRun code (i + 1) p (insert p (fromEnum c') m) bracketMap
+            | code!!i == '[' = if (findWithDefault 0 p m) == 0
+                then goRun code (bracketMap ! i) p m bracketMap
+                else goRun code (i + 1) p m bracketMap
+            | code!!i == ']' = if (findWithDefault 0 p m) /= 0
+                then goRun code (bracketMap ! i) p m bracketMap
+                else goRun code (i + 1) p m bracketMap
+            | otherwise = goRun code (i + 1) p m bracketMap
